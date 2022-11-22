c0f1ba440f3d79d76ed60b1a49673541d764de53
mineblock11 <93472213+mineblock11@users.noreply.github.com>
2022-11-22 19:47:34 +0000

    Potentially fix #18?

--- forge/src/main/java/mine/block/bolt/forge/mixin/client/ForgeClientMixin.java
+++ forge/src/main/java/mine/block/bolt/forge/mixin/client/ForgeClientMixin.java
@@ -1,6 +1,7 @@
 package mine.block.bolt.forge.mixin.client;
 
 
+import mine.block.bolt.config.BoltConfig;
 import net.minecraft.client.network.ServerInfo;
 import net.minecraft.server.ServerMetadata;
 import net.minecraftforge.client.ForgeHooksClient;
@@ -11,6 +12,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ForgeHooksClient.class)
 public class ForgeClientMixin {
+    @Inject(method = "createWorldConfirmationScreen", at = @At("HEAD"), cancellable = true, remap = false)
+    private static void bolt$createWorldConfigmationScreen(Runnable doConfirmedWorldLoad, CallbackInfo ci) {
+        if(BoltConfig.disableExperimentalWarning.get()) {
+            ci.cancel();
+            doConfirmedWorldLoad.run();
+        }
+    }
+
     @Inject(method = "processForgeListPingData", at = @At("HEAD"), cancellable = true)
     private static void bolt$processForgeListPingData(ServerMetadata oldPacket, ServerInfo serverData, CallbackInfo ci) {
         if (oldPacket.getBrandData() != null) {
